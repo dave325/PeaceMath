c371da8c5015ccfd576ba73ebfe9e4726bf3498d
Kelvin Lui <kelvin97lui@gmail.com>
2019-11-22 17:09:17 -0500

    change the buttons and input fields

--- peacemathWeb/static/index.js
+++ peacemathWeb/static/index.js
@@ -1,37 +1,5 @@
 'use strict';
 
-
-// const inputParameters = [8, 105, 111, 202];
-// inputParameters.forEach(value => {
-// 	document.querySelector(`.input-value_${value}`).addEventListener('click', function(){
-// 		// document.querySelector("#dropdown_input-value").innerHTML = value;
-// 		// inputParamValue = value;
-// 		_sendInputParams(inputParamValue);
-// 	});
-// });
-
-// /**
-//  * Makes POST request to set input parameter values of either:
-//  * 	8, 105, 111, 202
-//  * 
-//  * @param {*} inputParamValue 
-//  */
-// function _sendInputParams(inputParamValue=8){
-// 	axios.post('http://localhost:8000/physics/sendInitialParameterValue/', {
-//     'value': inputParamValue
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// }
-
-
-
-
-
 document.getElementById('btn_calculate').addEventListener('click', function(){
 	console.log(window.sessionStorage.getItem('key'))
 		fetch('/physics/chart/', {method:'post', headers: {
